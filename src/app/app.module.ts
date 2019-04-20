86e8c7e904c2d8d30f6f636a5cae722fc2236b4d
comeoninc <43278368+comeoninc@users.noreply.github.com>
2019-04-20 10:00:20 +0700

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
-@NgModule({
-    imports: [
-        MDBBootstrapModule.forRoot()
-    ],
-    schemas: [ NO_ERRORS_SCHEMA ]
-});
-
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -20,6 +14,7 @@ import { AppComponent } from './app.component';
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
